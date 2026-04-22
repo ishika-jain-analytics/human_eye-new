5b975acbd94e07db20ce0c1e77bfb87b11afc6cd
Bhumi Jain <bhumijain7533@gmail.com>
2026-04-22 14:34:34 +0530

    app

--- app.py
+++ app.py
@@ -1253,7 +1253,5 @@ def logout():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
